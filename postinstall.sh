b99029721ea8bccd9be2099bc22afc2a86ea4c86
Selgy <imselgy@gmail.com>
2023-12-09 02:12:31 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Define the Adobe CEP extensions directory
-CEP_EXTENSIONS_DIR="$HOME/Library/Application Support/Adobe/CEP/extensions"
+# Define the Adobe CEP extensions directory for the current user
+CEP_EXTENSIONS_DIR="/Library/Application Support/Adobe/CEP/extensions"
 
-# Define the source directory of PymiereLink (adjust this path as needed)
+# Define the source directory of PymiereLink (relative to the installation path)
 SOURCE_DIR="$1/Contents/Resources/PymiereLink"
 
-# Create the target directory if it doesn't exist
-mkdir -p "$CEP_EXTENSIONS_DIR"
+# Check if the source directory exists
+if [ -d "$SOURCE_DIR" ]; then
+    # Create the target directory if it doesn't exist
+    mkdir -p "$CEP_EXTENSIONS_DIR/PymiereLink"
 
-# Copy PymiereLink to the Adobe CEP extensions directory
-cp -R "$SOURCE_DIR" "$CEP_EXTENSIONS_DIR/PymiereLink"
+    # Copy PymiereLink to the Adobe CEP extensions directory
+    cp -R "$SOURCE_DIR" "$CEP_EXTENSIONS_DIR"
+
+    echo "PymiereLink installed successfully."
+else
+    echo "Error: PymiereLink source directory not found."
+    exit 1
+fi
