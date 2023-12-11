b9b879041362f10e65b0b55e6426d3aeec366bae
Selgy <imselgy@gmail.com>
2023-12-11 20:37:02 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew is not installed. Please install Homebrew first."
+    exit 1
+fi
+
+# Update Homebrew and upgrade any already-installed formulae
+brew update
+brew upgrade
+brew install python@3.8
+brew install gettext
+
 # Define the Adobe CEP extensions directory for the current user
 CEP_EXTENSIONS_DIR="$HOME/Library/Application Support/Adobe/CEP/extensions"
 
